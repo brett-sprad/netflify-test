d1fc3919d38273006b677a20e167b879feaae64a
Brett Spradbury <brett.spradbury@hideandseek.digital>
2023-05-05 15:06:22 +1000

    try fix test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://0.0.0.0:8080',
+    baseUrl: 'http://localhost:3000',
   },
   component: {
     devServer: {
